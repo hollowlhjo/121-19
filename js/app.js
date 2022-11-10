31efa2205b01a485a5f17b39d777735eda266558
holowlhjo <holowl.max@gmail.com>
2022-11-10 17:43:09 +0200

    com

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$('.spoiler').hide();
+
+$('.spoilerButton').on('click', function () {
+  $('.spoilerButton').hide();
+  $('.spoiler').slideDown(2000);
+});
+
+$(".spoiler2").hide();
+
+$(".spoilerButton2").on("click", function(){
+  $(".spoilerButton2").hide();
+  $(".spoiler2").slideDown(2000);
+});
